fcf53607effa903c981f3897a5f7664642b573e6
Laura Portillo <lauraportillo.ark@gmail.com>
2022-01-10 13:58:30 +0100

    add styles to title and subtitle with styled-components

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -14,20 +14,40 @@ const Container = styled.div`
   grid-template-rows: auto 1fr auto;
 `;
 
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-family: 'Nixie One', cursive;
+  font-size: 30px;
+  color: #fff;
+  text-shadow: 0 0 7px #fff, 0 0 10px #fff, 0 0 21px #fff, 0 0 42px red, 0 0 82px red, 0 0 92px red, 0 0 102px red,
+    0 0 151px red;
+`;
+
+const Subtitle = styled.h2`
+  font-family: 'Open Sans', sans-serif;
+  font-size: 15px;
+  color: #fff;
+`;
+
 const Landing = () => {
   return (
     <Container>
-      <main className="home">
-        <div className="home__image"></div>
-        <h1 className="home__title">trivial</h1>
-        <h2>🎲 Welcome to my react.js trivial number quiz!</h2>
+      <Main>
+        <Title>trivial</Title>
+        <Subtitle>Welcome to my react.js trivial number quiz!</Subtitle>
 
         <button className="home__btn">
           <Link to="/instructionsEn">
             <img className="home__btn--img" src={icon} alt="start button" />
           </Link>
         </button>
-      </main>
+      </Main>
       <Footer />
     </Container>
   );
