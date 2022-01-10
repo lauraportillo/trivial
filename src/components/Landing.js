3acd6113311c41b47958edcbc29aa4e2c933c045
Laura Portillo <lauraportillo.ark@gmail.com>
2022-01-10 13:19:24 +0100

    start using styled components

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,12 +7,20 @@ import styled from 'styled-components';
 // Images
 import icon from '../images/button-white.png';
 
+const Container = styled.div`
+  background-color: black;
+  min-height: 100vh;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+`;
+
 const Landing = () => {
   return (
-    <div className="containerHome">
+    <Container>
       <main className="home">
         <div className="home__image"></div>
-        <h1 className="home__title">trivial!</h1>
+        <h1 className="home__title">trivial</h1>
+        <h2>🎲 Welcome to my react.js trivial number quiz!</h2>
 
         <button className="home__btn">
           <Link to="/instructionsEn">
@@ -21,7 +29,7 @@ const Landing = () => {
         </button>
       </main>
       <Footer />
-    </div>
+    </Container>
   );
 };
 
