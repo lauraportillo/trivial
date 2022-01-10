9b557e79ede61847472ed948606de99cefd4f989
Laura Portillo <lauraportillo.ark@gmail.com>
2022-01-10 16:27:15 +0100

    clean footer and add styles with styled components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,55 +1,58 @@
 // Styles
 import styled from 'styled-components';
 
-const Footer = (props) => {
+const Container = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 40px;
+  font-size: 14px;
+  font-family: 'Roboto Condensed', sans-serif;
+  font-weight: 200;
+`;
+
+const Item = styled.li`
+  text-transform: uppercase;
+  margin-left: 5px;
+  margin-right: 5px;
+  list-style: none;
+  text-decoration: none;
+`;
+
+const Icon = styled.a`
+  color: white;
+`;
+
+const Footer = () => {
   return (
     <footer>
-      <ul className="footer">
-        <li className="footer__item">
-          <a
-            className="footer__item--icon"
+      <Container>
+        <Item>
+          <Icon
             href="https://www.linkedin.com/in/laura-portillo-rodr%C3%ADguez-21965a86/"
             target="_blank"
             rel="noopener noreferrer"
             title="linkedin"
           >
             <i className="fa fa-linkedin" alt="linkedin icon"></i>
-          </a>
-        </li>
-        <li className="footer__item">
-          <a
-            className="footer__item--icon"
-            href="https://github.com/lauraportillo"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="github"
-          >
+          </Icon>
+        </Item>
+        <Item>
+          <Icon href="https://github.com/lauraportillo" target="_blank" rel="noopener noreferrer" title="github">
             <i className="fa fa-github-alt" alt="github icon"></i>
-          </a>
-        </li>
-        <li className="footer__item">
-          <a
-            className="footer__item--icon"
-            href="https://twitter.com/LauraPo02860847"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="twitter"
-          >
+          </Icon>
+        </Item>
+        <Item>
+          <Icon href="https://twitter.com/LauraPo02860847" target="_blank" rel="noopener noreferrer" title="twitter">
             <i className="fa fa-twitter" alt="twitter icon"></i>
-          </a>
-        </li>
-        <li className="footer__item">
-          <a
-            className="footer__item--icon"
-            href="mailto:lauraportillo.ark@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="email"
-          >
+          </Icon>
+        </Item>
+        <Item>
+          <Icon href="mailto:lauraportillo.ark@gmail.com" target="_blank" rel="noopener noreferrer" title="email">
             <i className="fa fa-envelope" alt="envelope icon"></i>
-          </a>
-        </li>
-      </ul>
+          </Icon>
+        </Item>
+      </Container>
     </footer>
   );
 };
