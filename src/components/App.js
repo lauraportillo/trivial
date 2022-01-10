9f2a5578c840652eeeac2812caf77a1383b5617f
Laura Portillo <lauraportillo.ark@gmail.com>
2022-01-10 17:05:43 +0100

    add global style

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,22 @@
 import { Route, Switch } from 'react-router-dom';
 // Components
 import Landing from './Landing';
+// Styles
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 const App = () => {
   return (
     <Switch>
       <Route exact path="/">
+        <GlobalStyle />
         <Landing />
       </Route>
     </Switch>
