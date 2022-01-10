70cb6de8db485a6bd8a1cb6d6a0f099db3e230de
Laura Portillo <lauraportillo.ark@gmail.com>
2022-01-10 12:41:47 +0100

    create new landing component

---
+++ src/components/Landing.js
@@ -0,0 +1,16 @@
+// React
+import { Link } from 'react-router-dom';
+// Components
+import Footer from './Footer';
+// Styles
+import styled from 'styled-components';
+
+const Landing = (props) => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default Landing;
