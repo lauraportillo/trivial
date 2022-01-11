59b3c58b6520168e71c2235de16fc627cec96268
Laura Portillo <lauraportillo.ark@gmail.com>
2022-01-11 11:14:54 +0100

    create a structure for trivial page

--- src/components/Trivial.js
+++ src/components/Trivial.js
@@ -5,11 +5,39 @@ import FooterBlack from './FooterBlack';
 // Styles
 import styled from 'styled-components';
 
+const Container = styled.div`
+  min-height: 100vh;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-family: 'Nixie One', cursive;
+  font-size: 30px;
+  font-weight: 600;
+  color: black;
+  margin-top: 30px;
+  @media (min-width: 768px) {
+    font-size: 50px;
+    margin-top: 40px;
+  }
+`;
+
 const Trivial = (props) => {
   return (
-    <>
-      <div>esto es una prueba</div>
-    </>
+    <Container>
+      <Header>
+        <Title>arts&trivial</Title>
+      </Header>
+      <main></main>
+      <FooterBlack />
+    </Container>
   );
 };
 
