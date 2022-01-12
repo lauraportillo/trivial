65a79ec9439222ef6e2d008ecd31ec45000b3b97
Laura Portillo <lauraportillo.ark@gmail.com>
2022-01-12 10:08:18 +0100

    create new end screen compo

---
+++ src/components/EndScreen.js
@@ -0,0 +1,16 @@
+// React
+import React from 'react';
+// Components
+import FooterBlack from './FooterBlack';
+// Styles
+import styled from 'styled-components';
+
+const EndScreen = (props) => {
+  return (
+    <>
+      <div>THE END</div>
+    </>
+  );
+};
+
+export default EndScreen;
