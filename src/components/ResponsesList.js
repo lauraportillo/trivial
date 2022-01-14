bc240e56295a110f681b4d492ab9a3732ca9e429
Laura Portillo <lauraportillo.ark@gmail.com>
2022-01-14 10:49:19 +0100

    create responses list compo

---
+++ src/components/ResponsesList.js
@@ -0,0 +1,14 @@
+// React
+import { Questions } from '../data/Questions';
+// Styles
+import styled from 'styled-components';
+
+const ResponsesList = (props) => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default ResponsesList;
